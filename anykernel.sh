c6396001eaa38f81e606a9e39ffdc9347a2f7566
degidevops <110740389+degidevops@users.noreply.github.com>
2022-09-20 22:37:37 +0700

    update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -17,7 +17,7 @@ supported.versions=11-13
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
